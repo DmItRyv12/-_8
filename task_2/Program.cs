59ef2ef32392c7186be418a4bd21e195cd0553a6
Дмитрий Минаков <m1nak0vd@yandex.ru>
2021-12-07 22:03:32 +0300

    Добавьте файлы проекта.

---
+++ task_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество цифр в файле");
+            int C = Convert.ToInt32(Console.ReadLine());
+            int K = 0;
+            Random random = new Random();
+            string path = "C:\\Users\\asus\\Desktop\\Обучение\\Автоматизация BIM\\Задание 8\\ задача 2.txt";
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                for (int i = 0; i <= C; i++)
+                {
+                    sw.WriteLine(random.Next(0, 50));
+                }
+            }
+            using (StreamReader sr = new StreamReader(path))
+            {
+                for (int i = 0; i <= C; i++)
+                {
+                    K = K + Int32.Parse(sr.ReadLine());
+                }
+            }
+            Console.WriteLine($"Сумма чисел в файле задача 2.txt : {K}");
+            Console.ReadKey();
+        }
+    }
+}
